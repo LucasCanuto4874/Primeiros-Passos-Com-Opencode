693f5c131eba13f9f258fc77e44d3c9e37d3b881
Lucas Canuto <lucas.canuto@sptech.school>
2026-08-17 22:53:50 -0300

    melhorando em 200% o site

--- src/shared/components/Footer/Footer.tsx
+++ src/shared/components/Footer/Footer.tsx
@@ -6,11 +6,16 @@ function Footer() {
     <footer className="footer">
       <div className="footer__content">
         <div className="footer__left">
-          <h3 className="footer__title">Gentle Beast</h3>
           <nav className="footer__nav">
             <Link to="/" className="footer__link">Home</Link>
             <Link to="/cart" className="footer__link">Cart</Link>
             <Link to="/faq" className="footer__link">FAQ</Link>
+            <Link to="/" className="footer__link">About</Link>
+            <Link to="/" className="footer__link">Contact</Link>
+            <Link to="/" className="footer__link">Terms</Link>
+            <Link to="/" className="footer__link">Privacy</Link>
+            <Link to="/" className="footer__link">Shipping</Link>
+            <Link to="/" className="footer__link">Returns</Link>
           </nav>
         </div>
         <p className="footer__copyright">© 2026 Gentle Beast. All rights reserved.</p>
